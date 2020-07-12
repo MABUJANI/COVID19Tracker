4b6a7a5f3da77bb6ac26ab5c6f182cf36e9cdd79
MABUJANI <mabujanishaik10@gmail.com>
2020-07-12 22:52:13 +0530

    Deployment

---
+++ build/precache-manifest.df5221b3ec66ea458de37d82072c3e0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c80bbfdbdddd2a2a1104cf4e40ab42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ea6def41f8322b32373",
+    "url": "/static/css/main.7e07a35a.chunk.css"
+  },
+  {
+    "revision": "2d1637a240fb6057512a",
+    "url": "/static/js/2.52637cd7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.52637cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea6def41f8322b32373",
+    "url": "/static/js/main.2954bb9e.chunk.js"
+  },
+  {
+    "revision": "660c23ab2274be3d7bfe",
+    "url": "/static/js/runtime-main.73b82c14.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
